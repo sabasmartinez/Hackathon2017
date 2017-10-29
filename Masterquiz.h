9e2990180223ba790ca21a809fb707eeb2be851c
Lai Le <laile@wlan-t-172-33-dhcp.CSUChico.EDU>
2017-10-28 17:07:55 -0700

    new file:   .Lai.cpp.swp 	new file:   .Masterquiz.h.swp 	copied:     Masterquiz.cpp -> Lai.cpp 	modified:   Masterquiz.cpp 	new file:   Masterquiz.h 	modified:   a.out

---
+++ Masterquiz.h
@@ -0,0 +1,21 @@
+#ifndef MASTERQUIZ_H
+#define MASTERQUIZ_H
+class quiz
+{
+public:
+int Red,Yellow,Blue,Violet,Orange,Green,Purple;
+int userChoice;
+void question1(); 
+void question2();
+void question3();
+void question4();
+void question5(); 
+void redSum();
+void yellowSum(); 
+void blueSum();
+void violentSum();
+void orangeSum();
+void greenSum();
+void purpleSum();
+};
+#endif
